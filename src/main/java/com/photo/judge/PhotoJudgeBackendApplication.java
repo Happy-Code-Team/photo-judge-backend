f4220520eef380e0749f7021c035ae85806069f4
聂文学 <jr_niewenxue@yonyou.com>
2025-06-17 21:01:16 +0800

    nwx：代码初始化，引入mybatis-plus，适配原版mybatis

--- src/main/java/com/photo/judge/PhotoJudgeBackendApplication.java
+++ src/main/java/com/photo/judge/PhotoJudgeBackendApplication.java
@@ -1,9 +1,11 @@
 package com.photo.judge;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.photo.judge.dao")
 public class PhotoJudgeBackendApplication {
 
 	public static void main(String[] args) {
