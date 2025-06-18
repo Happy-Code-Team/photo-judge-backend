19ea7514653790490880f8f9da673611e07fdf17
聂文学 <65545471+414394568@users.noreply.github.com>
2025-06-18 15:34:21 +0800

    202506181516nwx：调整CarTestController代码
    202506181516nwx：调整CarTestController代码

--- src/main/java/com/photo/judge/controller/cartest/CarTestController.java
+++ src/main/java/com/photo/judge/controller/cartest/CarTestController.java
@@ -1,6 +1,5 @@
 package com.photo.judge.controller.cartest;
 
-import com.github.yulichang.wrapper.MPJLambdaWrapper;
 import com.photo.judge.model.entity.cartest.CarTestExtend;
 import com.photo.judge.service.cartest.CarTestService;
 import jakarta.annotation.Resource;
@@ -19,24 +18,11 @@ public class CarTestController {
 	@Resource
 	private CarTestService carTestService;
 
-	/** 测试 @SneakyThrows 注解的返回信息(统一异常处理)**/
-	@RequestMapping(value = "/cartest/list")
-	@ResponseBody
-	public List<CarTestExtend> list() {
-		List<CarTestExtend> list = carTestService.list();
-		return list;
-	}
-
-	/** 测试 @SneakyThrows 注解的返回信息(统一异常处理)**/
+	/** 根据id集合查询 **/
 	@RequestMapping(value = "/cartest/selectByIdList")
 	@ResponseBody
-	public List<CarTestExtend> selectByIdList(@RequestBody List<String> idList) {
-		MPJLambdaWrapper<CarTestExtend> wrapper = new MPJLambdaWrapper<>();
-		wrapper.in(CarTestExtend::getId, idList);
-		List<CarTestExtend> list1 = carTestService.list(wrapper);
-
-		List<CarTestExtend> list2 = carTestService.lambdaQuery().in(CarTestExtend::getId, idList).list();
-		return list1;
+	public List<CarTestExtend> selectByIdList(@RequestBody CarTestExtend carTestExtend) {
+		return carTestService.lambdaQuery().in(CarTestExtend::getId, carTestExtend.getIdList()).list();
 	}
 
 }
