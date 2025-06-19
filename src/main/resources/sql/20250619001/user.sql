daa3ceced4ca70563311d651e2aea27c86f10ae6
seaflower <1204378021@qq.com>
2025-06-19 15:12:20 +0800

    提交 user 建表语句

---
+++ src/main/resources/sql/20250619001/user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user (
+                      id varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '主键',
+                      user_code varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户编码',
+                      user_name varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '昵称',
+                      user_password varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户密码',
+                      user_avatar varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户头像链接',
+                      creater varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '创建人 ',
+                      create_time varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '创建时间',
+                      updater varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '修改人',
+                      update_time varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '修改时间',
+                      PRIMARY KEY (id) using BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
