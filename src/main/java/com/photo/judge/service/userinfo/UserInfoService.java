885f66cd0ef228e3d82e3e11f47ac18784411cde
seaflower <58713867+blueair5@users.noreply.github.com>
2025-06-23 16:29:09 +0800

    增加注册接口的重复校验和接口的注释 (#38)
    增加注册接口的重复校验和接口的注释 (#38)

--- src/main/java/com/photo/judge/service/userinfo/UserInfoService.java
+++ src/main/java/com/photo/judge/service/userinfo/UserInfoService.java
@@ -11,8 +11,10 @@ public interface UserInfoService extends MyService<UserInfoExtend> {
 	// 查询当前 userCode 是否存在
 	Boolean checkUserCodeIsExist(String userCode);
 
+	// 用户注册
 	Response userRegister(UserInfoExtend userInfoExtend);
 
+	// 用户登录
 	Response userLogin(UserInfoExtend userInfoExtend);
 
 }
