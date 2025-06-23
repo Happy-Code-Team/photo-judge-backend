e904a48fcdabb167dcea394fa6e2f25635715844
seaflower <1204378021@qq.com>
2025-06-23 11:51:37 +0800

    增加登录和注册功能

--- src/main/java/com/photo/judge/controller/userinfo/UserInfoController.java
+++ src/main/java/com/photo/judge/controller/userinfo/UserInfoController.java
@@ -1,6 +1,13 @@
 package com.photo.judge.controller.userinfo;
 
+import com.photo.judge.common.annotation.Desc;
+import com.photo.judge.common.response.Response;
+import com.photo.judge.model.entity.userinfo.UserInfoExtend;
+import com.photo.judge.service.userinfo.UserInfoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 /**
  * 用户信息表controller
@@ -8,5 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class UserInfoController {
+
+	private final UserInfoService userInfoService;
+
+	public UserInfoController(UserInfoService userInfoService) {
+		this.userInfoService = userInfoService;
+	}
+
+	@GetMapping("/user/checkUserCodeIsExist")
+	@Desc("检查用户编码是否存在")
+	public Response checkUserCodeIsExist(String userCode) {
+		if (userInfoService.checkUserCodeIsExist(userCode)) {
+			return Response.fail(500, "用户编码已存在");
+		} else {
+			return Response.success(null);
+		}
+	}
+
+	@PostMapping("/user/register")
+	@Desc("用户注册")
+	public Response register(@RequestBody UserInfoExtend userInfoExtend) {
+		return userInfoService.userRegister(userInfoExtend);
+	}
+
+	@PostMapping("/user/login")
+	@Desc("用户登录")
+	public Response login(@RequestBody UserInfoExtend userInfoExtend) {
+		log.info("User login attempt: {}", userInfoExtend);
+		return userInfoService.userLogin(userInfoExtend);
+	}
+
 
 }
