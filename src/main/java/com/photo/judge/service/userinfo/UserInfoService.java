46439273856535032dcc6d7434d07b50ace8e1dd
seaflower <58713867+blueair5@users.noreply.github.com>
2025-06-23 11:53:37 +0800

    增加登录和注册功能 (#24)
    增加登录和注册功能 (#24)

--- src/main/java/com/photo/judge/service/userinfo/UserInfoService.java
+++ src/main/java/com/photo/judge/service/userinfo/UserInfoService.java
@@ -1,5 +1,6 @@
 package com.photo.judge.service.userinfo;
 
+import com.photo.judge.common.response.Response;
 import com.photo.judge.model.entity.userinfo.UserInfoExtend;
 import com.photo.judge.service.myservice.MyService;
 
@@ -7,5 +8,11 @@ import com.photo.judge.service.myservice.MyService;
  * 用户信息表service
  */
 public interface UserInfoService extends MyService<UserInfoExtend> {
+	// 查询当前 userCode 是否存在
+	Boolean checkUserCodeIsExist(String userCode);
+
+	Response userRegister(UserInfoExtend userInfoExtend);
+
+	Response userLogin(UserInfoExtend userInfoExtend);
 
 }
--- src/main/resources/sql/20250619001/user_info.sql
+++ src/main/resources/sql/20250619001/user_info.sql
@@ -1,12 +1,13 @@
 CREATE TABLE user_info (
-    id varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '主键',
-    user_code varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户编码',
-    user_name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户昵称',
-    user_password varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户密码',
-    user_avatar varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户头像链接',
-    creater varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '创建人 ',
-    create_time varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '创建时间',
-    updater varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '修改人',
-    update_time varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '修改时间',
-    PRIMARY KEY (id) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
+                             id varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '主键',
+                             user_code varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户编码',
+                             user_name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户昵称',
+                             user_password varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户密码',
+                             user_avatar varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户头像链接',
+                             creater varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '创建人 ',
+                             create_time varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '创建时间',
+                             updater varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '修改人',
+                             update_time varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '修改时间',
+                             ts varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '最近修改时间',
+                             PRIMARY KEY (id) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='用户信息表';
